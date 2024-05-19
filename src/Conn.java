c1f7ea538954ffcbc93b3750275db32dad01cf1a
Ayush Garg <ayushgr1128@gmail.com>
2024-05-19 19:15:20 +0530

    University Management System

--- src/Conn.java
+++ src/Conn.java
@@ -8,7 +8,7 @@ public class Conn {
     Conn(){
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/universitymanagementsystem", "root", "A2004@garg#");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/universitymanagementsystem", "root", ""); // Do forget to put your MySQL Workbench password in the password field (empty field) if you have
             s = c.createStatement();
         }catch(Exception e){
             // e.printStackTrace();
